10851825fa53e961e25ebc43ae1b62f22047afac
yahyaefendi <54567084+yahyaefendi@users.noreply.github.com>
2022-03-31 14:21:19 +0700

    Add files via upload

---
+++ theme/js/medsos.js
@@ -0,0 +1,14 @@
+var medsos = [
+	[
+		'https://www.linkedin.com/in/rian-tri-kusuma-010683119/',
+		'theme/img/icon/icon_in.png',
+	],
+	[
+		'https://www.facebook.com/trikusumarian',
+		'theme/img/icon/icon_fb.png',
+	],
+	[
+		'https://www.instagram.com/riantk/',
+		'theme/img/icon/icon_ig.png',
+	],
+]
---
+++ theme/js/menu.js
@@ -0,0 +1,32 @@
+var menu = [
+	[
+		'home',
+		'fa fa-home',
+		'Home'
+	],
+	[
+		'about_me',
+		'fa fa-user',
+		'About Me'
+	],
+	[
+		'my_education',
+		'fas fa-graduation-cap',
+		'My Education'
+	],
+	[
+		'my_job_experience',
+		'fas fa-briefcase',
+		'My Job Experience'
+	],
+	[
+		'my_portofolio',
+		'fab fa-squarespace',
+		'My Portofolio'
+	],
+	[
+		'contact_me',
+		'fa fa-phone',
+		'Contact Me'
+	],
+]
